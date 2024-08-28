ac687949c360a16bb36573dbdab4ce6ff24cdc93
Ahmed Hasan <ahmed.h_199438@yahoo.com>
2024-08-28 07:39:28 +0300

    cart-err

---
+++ src/services/cartService.ts
@@ -0,0 +1,27 @@
+import { cartModel } from "../models/cartModel";
+
+interface CreateCartFoUser {
+  userId: string;
+}
+
+const createCartForUser = async ({ userId }: CreateCartFoUser) => {
+  const cart = await cartModel.create({ userId, totalAmount: 0 });
+  await cart.save();
+  return cart;
+};
+
+interface GetActiveCartForUser {
+  userId: string;
+}
+
+export const getActiveCartForUser = async ({
+  userId,
+}: GetActiveCartForUser) => {
+  let cart = await cartModel.findOne({ userId, status: "active" });
+
+  if (!cart) {
+    cart = await createCartForUser({ userId });
+  }
+
+  return cart;
+};
