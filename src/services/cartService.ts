038f563873373a66969eacbf7d56637bda9e97ad
Ahmed Hasan <ahmed.h_199438@yahoo.com>
2024-08-30 16:16:29 +0300

    Add item to cart / cart / items POST endpoint

--- src/services/cartService.ts
+++ src/services/cartService.ts
@@ -1,4 +1,5 @@
 import { cartModel } from "../models/cartModel";
+import productModel from "../models/productModel";
 
 interface CreateCartFoUser {
   userId: string;
@@ -24,4 +25,50 @@ export const getActiveCartForUser = async ({
   }
 
   return cart;
+};
+
+interface AddItemToCart {
+  productId: any;
+  quantity: number;
+  userId: string;
+}
+export const addItemToCart = async ({
+  productId,
+  quantity,
+  userId,
+}: AddItemToCart) => {
+  const cart = await getActiveCartForUser({ userId });
+
+  //Does the item exist in the cart?
+  const existsInCart = cart.items.find(
+    (p) => p.product.toString() === productId
+  );
+
+  if (existsInCart) {
+    return { data: "Item already exists in cart!", statusCode: 400 };
+  }
+
+  //fetch the product
+  const product = await productModel.findById(productId);
+
+  if (!product) {
+    return { data: "Product not found!", statusCode: 400 };
+  }
+
+  if (product.stock < quantity) {
+    return { data: "Not enough stock!", statusCode: 400 };
+  }
+
+  cart.items.push({
+    product: productId,
+    unitPrice: product.price,
+    quantity,
+  });
+
+  //Update the totalAmount for the cart
+  cart.totalAmount += product.price * quantity;
+
+  const updatedCart = await cart.save();
+
+  return { data: updatedCart, statusCode: 200 };
 };
