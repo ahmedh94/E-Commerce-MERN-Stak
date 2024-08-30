ec5840cf38db71523f81d08d21d481e8c3cd9f17
Ahmed Hasan <ahmed.h_199438@yahoo.com>
2024-08-30 04:30:44 +0300

    cart fix it

--- src/middlewares/validateJWT.ts
+++ src/middlewares/validateJWT.ts
@@ -1,10 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import userModel from "../models/userModel";
-
-interface ExtendRequest extends Request {
-  user?: any;
-}
+import { ExtendRequest } from "../types/extendRequest";
 
 const validateJWT = (req: ExtendRequest, res: Response, next: NextFunction) => {
   const authorizationHeader = req.get("authorization");
--- src/routes/cartRoute.ts
+++ src/routes/cartRoute.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import { getActiveCartForUser } from "../services/cartService";
 import validateJWT from "../middlewares/validateJWT";
+import { ExtendRequest } from "../types/extendRequest";
 
 const router = express.Router();
 
-router.get("/", validateJWT, async (req, res) => {
-  const userId = req.user._id;
+router.get("/", validateJWT, async (req: ExtendRequest, res) => {
+  const userId = req?.user?._id;
   const cart = await getActiveCartForUser({ userId });
   res.status(200).send(cart);
 });
