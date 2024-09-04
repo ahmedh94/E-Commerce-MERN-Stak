118a08c23fd78e0d0d056e54c39380a4a0e375a3
Ahmed Hasan <ahmed.h_199438@yahoo.com>
2024-09-05 01:24:52 +0300

    2

--- src/routes/productRoute.ts
+++ src/routes/productRoute.ts
@@ -8,11 +8,7 @@ router.get("/", async (req, res) => {
     const prouducts = await getAllProducts();
     res.status(200).send(prouducts);
   } catch (err) {
-<<<<<<< HEAD
     res.status(500).send("Something wes Wrong!");
-=======
-    res.status(500).send("Somthing went wrong!");
->>>>>>> c8b7283b583cf52c2346d78cf4bfe816e518154b
   }
 });
 
