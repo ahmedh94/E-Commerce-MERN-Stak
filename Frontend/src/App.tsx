71ccad6af60bda4d010195c018c7625886b99fee
Ahmed Hasan <ahmed.h_199438@yahoo.com>
2024-09-10 07:33:03 +0300

    front end chang - 8/9/2024

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import HomePage from "./pages/home"
 import Navbar from "./components/navbar"
+import RegisterPage from "./pages/registerPage"
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ Frontend/src/pages/registerPage.tsx
@@ -0,0 +1,56 @@
+import { Box, Button, TextField, Typography } from "@mui/material";
+import Container from "@mui/material/Container";
+import { useRef } from "react";
+import { BASE_URL } from "../constants/baseUrl";
+
+const RegisterPage = () => {
+    const firstNameRef = useRef<HTMLInputElement>(null);
+     const lastNameRef = useRef<HTMLInputElement>(null);
+     const emailRef = useRef<HTMLInputElement>(null);
+     const passwordRef = useRef<HTMLInputElement>(null);
+
+     const onSubmit = async () => {
+        const firstName = firstNameRef.current?.value;
+        const lastName = lastNameRef.current?.value;
+        const email = emailRef.current?.value;
+        const password = passwordRef.current?.value;
+
+        console.log(firstName, lastName, email, password);
+
+        //make the call to api to create the user
+        const response = await fetch(`${BASE_URL}/user/register`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                firstName,
+                lastName,
+                email,
+                password,
+            })
+        })
+        const data = await response.json();
+        console.log(data);
+     }
+
+    
+
+    return (
+        <Container>
+            <Box sx={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", mt: 4,}}>
+                <Typography variant="h4">Register New Account</Typography>
+                <Box sx={{display: "flex", flexDirection: "column", gap: 2, mt: 2}}>
+                <TextField inputRef={firstNameRef} label="First  Name" name="firstName" />
+                    <TextField inputRef={lastNameRef} label="Last Name" name="lastName" />
+                    <TextField inputRef={emailRef} type="email" label="Email" name="email" />
+                    <TextField inputRef={passwordRef} type="password" label="Passwoed" name="password" />
+                    <Button onClick={onSubmit} variant="contained">Register</Button>
+                </Box>
+            </Box>
+            
+        </Container>
+    )
+}
+
+export default RegisterPage;
